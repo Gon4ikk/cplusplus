bda8fd47b99a4c43acd86995c853d5e0424542b1
Gon4ikk <114654659+Gon4ikk@users.noreply.github.com>
2023-09-18 15:40:45 +0300

    firstlab_C++ commit

---
+++ firstlabc++.cpp
@@ -0,0 +1,18 @@
+﻿#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a = 10, b = 0.5, c = 1, d = 12.21, resultOne, resultTwo;
+
+	resultOne = (0.314 * exp(a) - 0.512 * exp(b)) / (sin(b / 3 * M_PI)) * log(a);
+
+	resultTwo = (0.81 * cbrt(c) - 1 / 2.125 * cbrt(d)) * exp(c);
+
+	cout << resultOne << endl;
+	cout << resultTwo << endl;
+	return 0;
+}
