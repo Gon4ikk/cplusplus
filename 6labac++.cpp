2b9e50c49d45950046b5f590de7c7ac7d57cc29a
Anton Honcharenko <114654659+Gon4ikk@users.noreply.github.com>
2023-11-01 13:10:27 +0200

    6 лаба

---
+++ 6labac++.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int N, product = 1;
+    std::cout << "Введіть натуральне число: ";
+    std::cin >> N;
+
+    while (N > 0) {
+        int digit = N % 10;
+        if (digit % 2 != 0) {
+            product *= digit;
+        }
+        N /= 10;
+    }
+
+    std::cout << "Добуток непарних цифр: " << product << std::endl;
+
+    return 0;
+}
